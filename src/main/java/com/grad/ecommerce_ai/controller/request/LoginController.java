98cad5608f8bc1dea4c45d43664d74eef9362ac7
marioqp1 <mario.sherif24@gmail.com>
2024-10-12 06:56:29 +0300

    ya rabbbb

--- src/main/java/com/grad/ecommerce_ai/controller/request/LoginController.java
+++ src/main/java/com/grad/ecommerce_ai/controller/request/LoginController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import static com.grad.ecommerce_ai.enitity.UserRoles.ROLE_COMPANY;
 
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/login")
 public class LoginController {

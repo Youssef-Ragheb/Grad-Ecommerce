890ea40332f2cec33a8f3785a518dce99b667cac
marioqp1 <mario.sherif24@gmail.com>
2024-10-12 06:38:15 +0300

    ya rabb

--- src/main/java/com/grad/ecommerce_ai/config/FrontConfig.java
+++ src/main/java/com/grad/ecommerce_ai/config/FrontConfig.java
@@ -6,12 +6,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class FrontConfig {
     @Configuration
-    public class WebConfig implements WebMvcConfigurer {
+    public static class WebConfig implements WebMvcConfigurer {
 
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-                    .allowedOrigins("*")
+                    .allowedOrigins("http://localhost:3000")
                     .allowedMethods("GET", "POST", "PUT", "DELETE")
                     .allowedHeaders("*")
                     .allowCredentials(true);

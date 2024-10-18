c720e82318c5eac4442e581f28fad8252165d7d2
marioqp1 <mario.sherif24@gmail.com>
2024-10-18 23:38:02 +0300

    fs

--- src/main/java/com/grad/ecommerce_ai/config/AppConfig.java
+++ src/main/java/com/grad/ecommerce_ai/config/AppConfig.java
@@ -1,17 +1,17 @@
-package com.grad.ecommerce_ai.config;
-
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class AppConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("http://localhost:3000") // Allow your frontend origin
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed methods
-                .allowCredentials(true); // Allow credentials if needed
-    }
-}
+//package com.grad.ecommerce_ai.config;
+//
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class AppConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**") // Allow all endpoints
+//                .allowedOrigins("http://localhost:3000") // Allow your frontend origin
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed methods
+//                .allowCredentials(true); // Allow credentials if needed
+//    }
+//}
